5500c35d1e56b943ff0c7db9c1121435debf0882
eduga3 <edulindorodrigues@gmail.com>
2022-10-29 23:16:04 -0300

    first commit

---
+++ src/main/java/br/com/spring/data/repository/CargoRepository.java
@@ -0,0 +1,10 @@
+package br.com.spring.data.repository;
+
+import br.com.spring.data.orm.Cargo;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CargoRepository extends CrudRepository<Cargo, Integer> {
+
+}
